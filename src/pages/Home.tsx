726bc171cc5af9facab8cfa188c2055f528ab0d6
SMcDani <Sbird7134@gmail.com>
2025-08-04 21:35:44 -0400

    Modified layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
 function Home() {
   return (
     <div>
-      <div className="bg-[url('/images/woman%203.jpg')] bg-cover md:bg-cover w-full h-screen">
-        <div className="bg-white w-auto">
-          <h1>Welcome to Lensify</h1>
+      <div className="flex bg-[url('/images/woman%203.jpg')] bg-cover w-full h-screen">
+        <div className="w-lg mx-4 my-auto p-8 bg-white rounded-xl xl:ml-40 md:mt-40 xl:w-170">
+          <div className="font-(family-name:--font-primary) text-5xl md:text-7xl">
+            <h1>Welcome</h1>
+            <h1>
+              to <span className="text-[#44457e]">Lensify</span>
+            </h1>
+          </div>
+          <p className="font-(family-name:--font-secondary) text-lg">
+            At Lensify, we specialize in high-quality stock lenses for optical
+            professionals. Browse our curated selection of lenses by brand,
+            power, and coating — ready to ship when you are.
+          </p>
+          <button className="border-[var(--lime)] border-solid">
+            Get started
+          </button>
         </div>
       </div>
     </div>
