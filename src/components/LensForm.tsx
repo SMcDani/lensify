3f7b1c2076d876e492b1c1320ca29457df0a7832
SMcDani <Sbird7134@gmail.com>
2025-08-25 21:02:13 -0400

    Added dropdown menus, modified padding and width of container for responsiveness

--- src/components/LensForm.tsx
+++ src/components/LensForm.tsx
@@ -1,5 +1,38 @@
 function LensForm() {
-  return <div>LensForm</div>;
+  return (
+    <div className="px-15">
+      <div className="flex flex-col">
+        <label htmlFor="material">Material</label>
+        <select id="material" name="material">
+          <option value="plastic">Plastic</option>
+          <option value="polycarbonate">Polycarbonate</option>
+          <option value="1.67">1.67</option>
+          <option value="1.74">1.74</option>
+          <option value="trivex">Trivex</option>
+          <option value="hivex">Hivex</option>
+        </select>
+        <label htmlFor="type">Type</label>
+        <select id="type" name="type">
+          <option value="both">Both Spherical/Aspherical</option>
+          <option value="spherical">Spherical</option>
+          <option value="aspherical">Aspherical</option>
+        </select>
+        <label htmlFor="coating">Coating</label>
+        <select id="coating" name="coating">
+          <option value="clear">Clear</option>
+          <option value="antireflective">Antireflective</option>
+          <option value="blue-block">Blue Blocker</option>
+        </select>
+        <label htmlFor="miscellaneous">Miscellaneous</label>
+        <select id="miscellaneous" name="miscellaneous">
+          <option value="none">None</option>
+          <option value="tr-grey">Transitions Grey</option>
+          <option value="tr-brown">Transitions Brown</option>
+          <option value="tr-xtractive">Transition XtrActive Grey</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default LensForm;
--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -3,8 +3,8 @@ import FormSet from "../components/FormSet";
 function Order() {
   return (
     <div className="h-screen">
-      <div className="w-fit mx-5 mt-[12vh] md:mx-auto rounded-sm shadow-xl overflow-hidden">
-        <div className="w-full mb-5">
+      <div className="w-fit mx-5 mt-[12vh] pb-10 md:w-160 md:mx-auto rounded-sm shadow-xl overflow-hidden">
+        <div className="w-full mb-10">
           <p className="h-10 bg-[var(--silver-lake-blue)] text-center text-white">
             Add to Cart
           </p>
