bfff45c7390ecd1868470489bc29a0b3980710a0
SMcDani <Sbird7134@gmail.com>
2025-10-22 22:28:00 -0400

    Set up backend to receive search query

--- src/components/LensForm.tsx
+++ src/components/LensForm.tsx
@@ -1,4 +1,33 @@
+import { useState, useEffect } from "react";
+
 function LensForm() {
+  //build the query using useState - will be used to search for lenses in db
+  const [query, setQuery] = useState({
+    material: "",
+    type: "",
+    coating: "",
+    miscellaneous: "",
+  });
+
+  //used to store the items that will be displayed
+  const [list, setList] = useState();
+
+  useEffect(() => {
+    //turn the query into a string
+    const fullQuery = new URLSearchParams(query).toString();
+
+    //make a call to the api using query
+    fetch(`http://localhost:3000/products?${fullQuery}`)
+      .then((res) => res.json()) //get the response object as usable JSON
+      .then((data) => setList(data));
+  }, [query]);
+
+  //take in the updated menu selection and update it in query
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setQuery((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className="px-15">
       <div className="flex flex-col">
@@ -6,7 +35,8 @@ function LensForm() {
         <select
           id="material"
           name="material"
-          className="mb-3 bg-[#cfe6ff] rounded-lg "
+          className="mb-3 bg-[#cfe6ff] rounded-lg"
+          onChange={handleChange}
         >
           <option value="plastic">Plastic</option>
           <option value="polycarbonate">Polycarbonate</option>
@@ -16,7 +46,12 @@ function LensForm() {
           <option value="hivex">Hivex</option>
         </select>
         <label htmlFor="type">Type</label>
-        <select id="type" name="type" className="mb-3 bg-[#cfe6ff] rounded-lg">
+        <select
+          id="type"
+          name="type"
+          className="mb-3 bg-[#cfe6ff] rounded-lg"
+          onChange={handleChange}
+        >
           <option value="both">Both Spherical/Aspherical</option>
           <option value="spherical">Spherical</option>
           <option value="aspherical">Aspherical</option>
@@ -26,6 +61,7 @@ function LensForm() {
           id="coating"
           name="coating"
           className="mb-3 bg-[#cfe6ff] rounded-lg"
+          onChange={handleChange}
         >
           <option value="clear">Clear</option>
           <option value="antireflective">Antireflective</option>
@@ -36,6 +72,7 @@ function LensForm() {
           id="miscellaneous"
           name="miscellaneous"
           className="mb-3 bg-[#cfe6ff] rounded-lg"
+          onChange={handleChange}
         >
           <option value="none">None</option>
           <option value="tr-grey">Transitions Grey</option>
